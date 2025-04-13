a16fcc71e500cfda05804f1658e7fb2a463de018
MATI22kjfhsds <bladenort@gmail.com>
2025-04-13 11:48:49 -0400

    Delete acl.py

--- acl.py
+++
@@ -1,8 +0,0 @@
-numero_acl = int(input("Ingrese el número de ACL IPv4: "))
-
-if 1 <= numero_acl <= 99:
-    print(f"ACL {numero_acl} es una ACL Estándar.")
-elif 100 <= numero_acl <= 199:
-    print(f"ACL {numero_acl} es una ACL Extendida.")
-else:
-    print(f"El número {numero_acl} no corresponde a una lista de acceso válida.")
